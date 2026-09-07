d85053d53be1344c1f0f5699d843e1aa192f7f42
nacsadominik11d <nacsa.dominik.2024d@telekimezotur.hu>
2026-09-07 14:27:53 +0200

    szorzas

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿int szam1 = 190;
 int szam2 = 19;
 
-if (szam1 < szam2) Console.WriteLine($"A nagyobb: {szam2}");
-else Console.WriteLine(szam1);
+if (szam1 < szam2)
+{
+    szam2 = szam2 * 2;
+    Console.WriteLine($"A nagyobb: {szam2}");
+}
+else
+
+{
+    szam1 = szam1 * 2;
+    Console.WriteLine(szam1);
+}
